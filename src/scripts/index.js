067f9adb16011c41437bc4bf72768a9520849532
Andrii Shtadler <andrii_shtadler@epam.com>
2019-12-25 01:05:45 +0200

    steps

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,16 @@ import { Interaction } from 'three.interaction';
 import { SpriteText2D, textAlign } from 'three-text2d'
 import { Sky } from './objects/sky';
 import { OrbitControls } from './contrlos/Orbit';
+import StepHelper from './step-helper';
+
+const STEPS = {
+  SHOW_HI: 'SHOW_HI',
+  POLAR_MOVE_START: 'POLAR_MOVE_START',
+  TEXT_TO_BACK: 'TEXT_TO_BACK',
+  POLAR_MOVE_END: 'POLAR_MOVE_END',
+  ANIMATE_CLICK_BTN: 'ANIMATE_CLICK_BTN',
+  CLICKED_PLAY: 'CLICKED_PLAY' 
+}
 
 class Main {
   // main entities
@@ -18,13 +28,16 @@ class Main {
   // positions
   cameraPosition = { x: 40000, y: 40000, z: 40000 }
 
+  // request animation frames here
+  requestsFrames = {}
 
   constructor() {
+    this.step = new StepHelper();
     this.init();
     this.initSky();
-    window.addEventListener('resize', this.onWindowResize, false);
-    this.clickToPlay();
-    this.a();
+    window.addEventListener('resize', this.onWindowResize.bind(this), false);
+    this.setClickToPlayEvents()
+    this.sequence()
   }
   
   render() {
@@ -39,17 +52,30 @@ class Main {
     const helper = new THREE.GridHelper(10000, 20, 0xffffff, 0xffffff);
     helper.visible = true;
     this.scene.add(helper);
-
-    this.sprite1 = new SpriteText2D("ANDRII SHTADLER", { align: textAlign.center, font: 'bold 1500px ArialBold', fillStyle: '#ffffff', antialias: false, shadowColor: '#ffffff' })
-    this.sprite2 = new SpriteText2D("Interactive portfolio", { align: textAlign.center, font: 'bold 1300px ArialBold', fillStyle: '#ffffff', antialias: false, shadowColor: '#ffffff' })
-    this.sprite3 = new SpriteText2D("CLICK TO PLAY", { align: textAlign.center, font: 'bold 1500px ArialBold', fillStyle: '#ffffff', antialias: false, shadowBlur: 50, shadowColor: '#ffffff' })
+    if(window.innerWidth > 520) {
+      this.sprite1 = new SpriteText2D("ANDRII SHTADLER", { align: textAlign.center, font: 'bold 1500px ArialBold', fillStyle: '#ffffff', antialias: false, shadowColor: '#ffffff' })
+      this.sprite2 = new SpriteText2D("Interactive portfolio", { align: textAlign.center, font: 'bold 1300px ArialBold', fillStyle: '#ffffff', antialias: false, shadowColor: '#ffffff' })
+      this.sprite3 = new SpriteText2D("CLICK TO PLAY", { align: textAlign.center, font: 'bold 1500px ArialBold', fillStyle: '#ffffff', antialias: false, shadowBlur: 50, shadowColor: '#ffffff' })
+    } else {
+      this.sprite1 = new SpriteText2D("ANDRII SHTADLER", { align: textAlign.center, font: 'bold 1200px ArialBold', fillStyle: '#ffffff', antialias: false, shadowColor: '#ffffff' })
+      this.sprite2 = new SpriteText2D("Interactive portfolio", { align: textAlign.center, font: 'bold 1000px ArialBold', fillStyle: '#ffffff', antialias: false, shadowColor: '#ffffff' })
+      this.sprite3 = new SpriteText2D("CLICK TO PLAY", { align: textAlign.center, font: 'bold 1500px ArialBold', fillStyle: '#ffffff', antialias: false, shadowBlur: 50, shadowColor: '#ffffff' })
+    }
+    this.sprite1.material.opacity = 0;
+    this.sprite2.material.opacity = 0;
 
     this.scene.add(this.sprite1)
     this.scene.add(this.sprite2)
     this.scene.add(this.sprite3)
 
-    this.sprite1.position.set(25000, 25000, 25000)
-    this.sprite2.position.set(25000, 23000, 25000)
+    if(window.innerWidth > 520) {
+      this.sprite1.position.set(25000, 25000, 25000)
+      this.sprite2.position.set(25000, 23000, 25000)
+    } else {
+      this.sprite1.position.set(25000, 25000, 25000)
+      this.sprite2.position.set(25000, 23500, 25000)
+    }
+
     this.sprite3.position.set(25000, 3000, 25000)
 
     this.renderer = new THREE.WebGLRenderer();
@@ -134,7 +160,7 @@ class Main {
     this.render();
   }
 
-  a() {
+  textMovie() {
     let currentPolar = this.controls.getPolarAngle()
     if(currentPolar >= Math.PI / 2.05) {
       this.sprite1.material.opacity > 0 && (this.sprite1.material.opacity -= 0.01);
@@ -158,15 +184,115 @@ class Main {
     }
     
     this.render()
-    requestAnimationFrame(()=> { this.a()})
+    requestAnimationFrame(()=> { this.textMovie()})
   }
 
-  clickToPlay() {
+  sequence() {
+    this.step.on(STEPS.SHOW_HI, () => {
+      this.stepShowHi(() => {
+        // start move the polar angle
+        this.step.go(STEPS.POLAR_MOVE_START)
+        this.step.go(STEPS.TEXT_TO_BACK)
+      })
+    })
+    this.step.on(STEPS.POLAR_MOVE_START, () => {
+      this.stepPolarStart(() => this.step.go(STEPS.POLAR_MOVE_END))
+    })
+    this.step.on(STEPS.TEXT_TO_BACK, () => {
+      this.stepTextBack()
+    })
+    this.step.on(STEPS.POLAR_MOVE_END, () => {
+      this.stepPolarEnd()
+      this.stepClickToPlay()
+    
+    })
+    this.step.go(STEPS.SHOW_HI)
+  }
+
+  stepShowHi(finish) {
+    if(this.sprite1.material.opacity >= 1) {
+      cancelAnimationFrame(this.requestsFrames[STEPS.SHOW_HI])
+
+      return finish()
+    };
+
+    this.sprite1.material.opacity += 0.007;
+    this.sprite2.material.opacity += 0.007;
+
+    this.render()
+    this.requestsFrames[STEPS.SHOW_HI] = requestAnimationFrame(()=> { this.stepShowHi(finish)})
+  }
+
+  stepPolarStart(finish) {
+    let currentPolar = this.controls.getPolarAngle()
+    if(currentPolar >= Math.PI / 2.05) {
+      cancelAnimationFrame(this.requestsFrames[STEPS.POLAR_MOVE_START])
+      return finish()
+    }
+
+    this.controls.rotateUp(-0.001)
+    this.render()
+    this.requestsFrames[STEPS.POLAR_MOVE_START] = requestAnimationFrame(()=> { this.stepPolarStart(finish)})
+  }
+
+  stepTextBack() {
+    if(this.sprite1.material.opacity <= 0) {
+      cancelAnimationFrame(this.requestsFrames[STEPS.TEXT_TO_BACK])
+      return
+    }
+
+    if(this.sprite1.position.y > 19000) {
+      this.sprite1.position.y -= 20
+      this.sprite2.position.y -= 20
+    } else {
+      this.sprite1.position.y -= 30
+      this.sprite1.position.x -= 80
+      this.sprite1.position.z -= 80
+      this.sprite2.position.y -= 30
+      this.sprite2.position.x -= 80
+      this.sprite2.position.z -= 80
+      this.sprite1.material.opacity -= 0.001
+      this.sprite2.material.opacity -= 0.001
+    };
+
+    this.render()
+    this.requestsFrames[STEPS.TEXT_TO_BACK] = requestAnimationFrame(() => { this.stepTextBack()})
+  }
+
+  stepPolarEnd() {
+    if(this.sprite1.material.opacity <= 0) {
+      cancelAnimationFrame(this.requestsFrames[STEPS.POLAR_MOVE_END])
+      return
+    }
+    this.sprite1.material.opacity > 0 && (this.sprite1.material.opacity -= 0.01);
+    this.sprite2.material.opacity > 0 && (this.sprite2.material.opacity -= 0.01);
+
+    this.requestsFrames[STEPS.POLAR_MOVE_END] = requestAnimationFrame(() => { this.stepPolarEnd()})
+  }
+  
+  playColor = new THREE.Color('#ffffff')
+  stepClickToPlay() {
+    console.log(this.playColor) 
+
+    if(this.playColor.b < 0.5) {
+      cancelAnimationFrame(this.requestsFrames[STEPS.ANIMATE_CLICK_BTN])
+      return;
+    }
+    this.playColor.g -= 0.03
+    this.playColor.b -= 0.03
+    this.sprite3.fillStyle = '#'+this.playColor.getHexString()
+    this.render()
+    this.requestsFrames[STEPS.ANIMATE_CLICK_BTN] = requestAnimationFrame(() => { this.stepClickToPlay()})
+  }
+
+  setClickToPlayEvents() {
     const hoverPlay = () => {
-      this.sprite3.fillStyle = '#ff9540'
+      this.sprite3.fillStyle = '#ff5555'
+      this.render()
     }
     const leavePlay = () => {
-      this.sprite3.fillStyle = '#ffffff'
+      this.sprite3.fillStyle = '#'+this.playColor.getHexString()
+      this.render()
     }
     this.sprite3.on('mouseover', hoverPlay);
     this.sprite3.on('mouseout', leavePlay);
@@ -175,6 +301,7 @@ class Main {
       this.sprite3.off('mouseout', leavePlay)
       
       this.sprite3.fillStyle = '#ffffff'
+      this.render()
     })
   }
 }
